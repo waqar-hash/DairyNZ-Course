5a265ccd14fade607cffee46e30fa97892e274f0
Waqar Akram <wakram@waikato.ac.nz>
2026-02-01 21:19:31 +1300

    install.packages("dplyr") install.packages("ggplot2") install.packages("tidyr") install.packages("readr")

--- R_Script_for_DairyNZ.R
+++ R_Script_for_DairyNZ.R
@@ -1,3 +1,8 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("tidyr")
+install.packages("readr")
